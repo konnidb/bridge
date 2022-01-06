63d1a8159b779d72cb854db7cfd61c83c5c14b34
Edgar <edgar21_9@hotmail.com>
2022-01-06 00:15:27 -0600

    Added comparisons module

---
+++ src/graph_engine/comparisons.rs
@@ -0,0 +1,11 @@
+pub fn greater_than() {
+
+}
+
+pub fn less_than() {
+    
+}
+
+pub fn equals() {
+
+}
--- src/graph_engine/mod.rs
+++ src/graph_engine/mod.rs
@@ -1,20 +1,40 @@
-use std::collections::HashMap;
+mod comparisons;
+use std::{collections::HashMap, str::Bytes, vec};
 
 use crate::schemas::Schema;
 
+pub use comparisons::*;
+
 // TODO Rethink lifecycles in this section
 
+#[derive(Debug)]
 pub struct Graph<'a> {
     is_directed: bool,
     nodes: Vec<Node<'a>>,
-    edges: Vec<Edge<'a>>
+    edges: Vec<Edge<'a>>,
 }
 
+impl Default for Graph<'static> {
+    fn default() -> Self {
+        Self {
+            is_directed: true,
+            nodes: vec![],
+            edges: vec![],
+        }
+    }
+}
+
+impl<'a> Graph<'a> {
+    
+}
+
+#[derive(Debug)]
 pub struct Node<'a> {
     schema: Schema<'a>,
-    data: HashMap<String, String>
+    data: HashMap<String, String>,
 }
 
+#[derive(Debug)]
 pub struct Edge<'a> {
     source: &'a Node<'a>,
     dest: &'a Node<'a>,
