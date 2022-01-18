70713f0e71461bd38469f25d4f43a0bcf3bee2ef
Edgar <edgar21_9@hotmail.com>
2022-01-18 00:51:03 -0600

    Added get_nodes from query

--- src/query_lang/mod.rs
+++ src/query_lang/mod.rs
@@ -2,14 +2,53 @@ use std::collections::HashMap;
 
 use crate::schemas::Schema;
 
+#[derive(Debug)]
 pub struct Match {
     nodes: Vec<String>,
-    vertices: Vec<String>
+    vertices: Vec<String>,
 }
 
 #[derive(Debug)]
 pub struct QueryNode<'a> {
     schema: Schema<'a>,
-    properties: HashMap<String,String>
+    properties: HashMap<String, String>,
+}
+
+impl Match {
+    pub fn new(nodes: Vec<String>, vertices: Vec<String>) -> Self {
+        Self { nodes, vertices }
+    }
+
+    
+}
+
+#[derive(Debug)]
+pub struct Query<'a> {
+    pub query: &'a String
+}
+
+impl<'a> Query<'a> {
+    pub fn new(query: &'a String) -> Self {
+        Self {
+            query
+        }
+    }
+
+    pub fn get_nodes(self) {
+        let mut tokens = self.query.split(" ");
+        let mut sentence_map: HashMap<&str, bool> = HashMap::new();
+
+        for token in tokens {
+            match token {
+                "match" => {},
+                "create" => {},
+                "delete" => {},
+                "update" => {},
+                "with" => {},
+                "|" => {},
+                _ => {},
+            }
+        }
+    }
 }
 
